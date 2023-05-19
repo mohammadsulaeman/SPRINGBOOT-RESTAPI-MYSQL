31cc159f47b3a7d4f1a4bf4593acbc56e7afea68
mohammadsulaeman <sulaemanm991@gmail.com>
2023-05-19 14:11:02 +0700

    perubahan extends

--- src/main/java/com/example/springboot/restapi/repository/EmployeeRepository.java
+++ src/main/java/com/example/springboot/restapi/repository/EmployeeRepository.java
@@ -1,8 +1,8 @@
 package com.example.springboot.restapi.repository;
 
 import com.example.springboot.restapi.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 
-public interface EmployeeRepository  extends JpaRepository<Employee,Long> {
+public interface EmployeeRepository  extends CrudRepository<Employee,Long> {
 }
